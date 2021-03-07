206bf033dd9a2b372b77091df7a1e106abaf74b8
frolkit <frolkit@gmail.com>
2021-03-07 19:24:14 +0300

    updating function and variable names

--- images/tests.py
+++ images/tests.py
@@ -15,41 +15,41 @@ class TestData(TestCase):
             b'\x02\x00\x01\x00\x00\x02\x02\x0C'
             b'\x0A\x00\x3B'
         )
-        cls.image = SimpleUploadedFile(name='img.jpeg',
-                                       content=image,
-                                       content_type='image/jpeg')
-        cls.link = 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png' # noqa
-        cls.invalid_link = 'https://www.google.com/'
+        cls.image_file = SimpleUploadedFile(name='img.jpeg',
+                                            content=image,
+                                            content_type='image/jpeg')
+        cls.image_link = 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png' # noqa
+        cls.invalid_image_link = 'https://www.google.com/'
 
 
 class ImageCreateErrors(TestData):
 
-    def test_upload_invalid_link(self):
+    def test_upload_with_invalid_link(self):
         response = self.client.post('/image_create/',
-                                    {'image_link': self.invalid_link})
+                                    {'image_link': self.invalid_image_link})
         self.assertContains(response, "не получилось найти изображение")
 
-    def test_send_no_input(self):
+    def test_upload_with_no_input(self):
         response = self.client.post('/image_create/', {'image_link': ''})
         self.assertContains(response, 'Не указан вариант загрузки.')
 
-    def test_send_two_input(self):
+    def test_upload_with_two_input(self):
         response = self.client.post('/image_create/',
-                                    {'image_link': self.link,
-                                     'image_file': self.image})
+                                    {'image_link': self.image_link,
+                                     'image_file': self.image_file})
         self.assertContains(response, 'Укажите только один вариант загрузки.')
 
 
 class ImageCreate(TestData):
 
-    def test_create_image_from_link(self):
+    def test_upload_image_from_link(self):
         response = self.client.post('/image_create/',
-                                    {'image_link': self.link})
+                                    {'image_link': self.image_link})
         self.assertEqual(response.status_code, 302)
 
-    def test_create_image_from_file(self):
+    def test_upload_image_from_file(self):
         response = self.client.post('/image_create/',
-                                    {'image_file': self.image})
+                                    {'image_file': self.image_file})
         self.assertEqual(response.status_code, 302)
 
 
@@ -59,7 +59,7 @@ class ImageEdit(TestData):
     def setUpClass(cls):
         super().setUpClass()
         cls.client.post('/image_create/',
-                        {'image_file': cls.image},
+                        {'image_file': cls.image_file},
                         follow=True)
 
     def test_edit_width(self):
@@ -78,7 +78,7 @@ class ImageEdit(TestData):
         width, height = edited_image.size
         self.assertEqual(height, 1080)
 
-    def test_edit_width_height(self):
+    def test_edit_width_and_height(self):
         response = self.client.post('/image_edit/1/',
                                     {'width': 1920,
                                      'height': 1080},
